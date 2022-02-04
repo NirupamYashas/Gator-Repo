d120a385563f83475c716a6d3db29a8f94a09539
Praveen Ravula <praveen@Praveens-MacBook-Pro.local>
2022-02-04 06:04:08 -0500

    add updated files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,31 @@
-import { ProjectsService } from './projects.service';
+import {RouterModule} from '@angular/router';
+import { ProjectsService } from './projects/projects.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectsComponent } from './projects.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateComponent } from './create/create.component';
+import { AvailableRoutes } from './app.routing';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    CreateComponent,
+    NavbarComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatListModule,
+    RouterModule,
+    RouterModule.forRoot(AvailableRoutes)
   ],
   providers: [
     ProjectsService
