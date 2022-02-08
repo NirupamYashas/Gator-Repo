576b5072e4588af3cfd74d9424cabf7370695132
jd4386 <scientistjagan2000@gmail.com>
2022-02-07 21:03:59 -0500

    sqlite database connection

--- server/main.go
+++ server/main.go
@@ -4,43 +4,95 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"strconv"
 
+	// "strconv"
+
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+type App struct {
+	db *gorm.DB
+	r  *mux.Router
+}
+
 type Project struct {
-	ID         string `json:"id"`
+	ID         string `gorm:"primaryKey" json:"id"`
 	Name       string `json:"name"`
 	Department string `json:"department"`
 	Email      string `json:"email"`
 	Link       string `json:"link"`
 }
 
+var app App
 var projects []Project
+var project Project
 
-func getProjects(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(projects)
+func (a *App) start() {
+	a.db.AutoMigrate(&Project{})
+
+	project = Project{ID: uuid.New().String(), Name: "Game Project", Department: "CISE", Email: "game@gmail.com", Link: "github.com/game"}
+	a.db.Create(&project)
+
+	project = Project{ID: uuid.New().String(), Name: "ML Project", Department: "CISE", Email: "ml@gmail.com", Link: "github.com/ml"}
+	a.db.Create(&project)
+
+	a.r.HandleFunc("/api/projects", a.getProjects).Methods("GET")
+	a.r.HandleFunc("/api/projects", a.addProject).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", a.r))
 }
 
-func addProject(w http.ResponseWriter, r *http.Request) {
+func (a *App) getProjects(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var project Project
-	_ = json.NewDecoder(r.Body).Decode(&project)
-	project.ID = strconv.Itoa(len(projects) + 1)
-	projects = append(projects, project)
-	json.NewEncoder(w).Encode(project)
+
+	err := a.db.Find(&projects).Error
+
+	if err != nil {
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(projects)
+
+	if err != nil {
+		json.NewEncoder(w).Encode(err.Error())
+	}
+}
+
+func (a *App) addProject(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewDecoder(r.Body).Decode(&project)
+
+	if err != nil {
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	project.ID = uuid.New().String()
+	err = a.db.Create(&project).Error
+
+	if err != nil {
+		json.NewEncoder(w).Encode(err.Error())
+	} else {
+		w.WriteHeader(http.StatusCreated)
+	}
 }
 
 func main() {
-	r := mux.NewRouter()
+	db, err := gorm.Open(sqlite.Open("gator-repo.db"), &gorm.Config{})
 
-	projects = append(projects, Project{ID: "1", Name: "Game Project", Department: "CISE", Email: "game@gmail.com", Link: "github.com/game"})
-	projects = append(projects, Project{ID: "2", Name: "ML Project", Department: "CISE", Email: "ml@gmail.com", Link: "github.com/ml"})
+	if err != nil {
+		panic(err.Error())
+	}
 
-	r.HandleFunc("/api/projects", getProjects).Methods("GET")
-	r.HandleFunc("/api/projects", addProject).Methods("POST")
+	app = App{
+		db: db,
+		r:  mux.NewRouter(),
+	}
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	app.start()
 }
