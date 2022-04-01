363cd2cec8dfc3f69af537f7207bff2af3c12d1e
jd4386 <45505677+jd4386@users.noreply.github.com>
2022-04-01 18:05:10 -0400

    restructured search project functionality

--- server/controllers/projects.go
+++ server/controllers/projects.go
@@ -6,6 +6,7 @@ import (
 	"server/cors"
 	"server/models"
 	"server/utilities"
+	"strings"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
@@ -132,6 +133,37 @@ func GetProjectsByDepartment(w http.ResponseWriter, r *http.Request) {
 
 	var projects []models.Project
 	err := utilities.App.DB.Table("projects").Find(&projects, "department = ?", mux.Vars(r)["department"]).Error
+
+	if err != nil {
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(projects)
+
+	if err != nil {
+		json.NewEncoder(w).Encode(err.Error())
+	}
+}
+
+func GetProjectsBySearch(w http.ResponseWriter, r *http.Request) {
+	cors.SetupCorsResponse(&w, r)
+	if (*r).Method == "OPTIONS" {
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	res := strings.Split(mux.Vars(r)["search_phrase"], " ")
+	tx := utilities.App.DB.Table("projects")
+
+	for _, element := range res {
+		search_term := "%" + element + "%"
+		tx = tx.Where("name LIKE ? OR email LIKE ?", search_term, search_term)
+	}
+
+	var projects []models.Project
+	err := tx.Find(&projects).Error
 
 	if err != nil {
 		json.NewEncoder(w).Encode(err.Error())
