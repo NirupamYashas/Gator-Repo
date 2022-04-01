9e69a50721c92976f321fddb61fc21b28d062f72
jd4386 <45505677+jd4386@users.noreply.github.com>
2022-04-01 18:02:12 -0400

    restructured add projects functionality

--- server/controllers/projects.go
+++ server/controllers/projects.go
@@ -6,6 +6,8 @@ import (
 	"server/cors"
 	"server/models"
 	"server/utilities"
+
+	"github.com/google/uuid"
 )
 
 func GetProjects(w http.ResponseWriter, r *http.Request) {
@@ -28,6 +30,34 @@ func GetProjects(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+}
+
+func AddProject(w http.ResponseWriter, r *http.Request) {
+	cors.SetupCorsResponse(&w, r)
+	if (*r).Method == "OPTIONS" {
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	var project models.Project
+	err := json.NewDecoder(r.Body).Decode(&project)
+
+	if err != nil {
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	project.ID = uuid.New().String()
+	err = utilities.App.DB.Table("projects").Save(&project).Error
+
+	if err != nil {
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	} else {
+		w.WriteHeader(http.StatusCreated)
 		return
 	}
 }
