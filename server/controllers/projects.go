ca7bfc69beff523ff505847423ecb9bba10d67e3
jd4386 <45505677+jd4386@users.noreply.github.com>
2022-04-01 18:04:27 -0400

    restructured department wise sorting functionality

--- server/controllers/projects.go
+++ server/controllers/projects.go
@@ -121,3 +121,26 @@ func DeleteProject(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+func GetProjectsByDepartment(w http.ResponseWriter, r *http.Request) {
+	cors.SetupCorsResponse(&w, r)
+	if (*r).Method == "OPTIONS" {
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	var projects []models.Project
+	err := utilities.App.DB.Table("projects").Find(&projects, "department = ?", mux.Vars(r)["department"]).Error
+
+	if err != nil {
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(projects)
+
+	if err != nil {
+		json.NewEncoder(w).Encode(err.Error())
+	}
+}
