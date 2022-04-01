83d4027c2e006e53b038edb119e9ad6cb2a49a69
NirupamYashas <kuchimanchi.yash1234@gmail.com>
2022-03-31 20:27:27 -0400

    updated auth guard file

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+  }
+
+  public get currentUserValue(): User {
+      return this.currentUserSubject.value;
+  }
 }
