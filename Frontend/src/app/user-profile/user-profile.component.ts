7806e75bbe5f36f8403450e2ce3f5f5c20c16602
NirupamYashas <48911001+NirupamYashas@users.noreply.github.com>
2022-04-02 20:01:10 -0400

    commit changes which are done before sprint-3

--- Frontend/src/app/user-profile/user-profile.component.ts
+++ Frontend/src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent implements OnInit{
 
-  constructor() { }
+  constructor(public authenticationService: AuthenticationService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+  ngOnDestroy() {
   }
 
 }
