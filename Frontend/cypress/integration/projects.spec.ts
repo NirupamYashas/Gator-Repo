152f2f1aa511c3bb7e8557c9c96cbc256eaf724e
NirupamYashas <48911001+NirupamYashas@users.noreply.github.com>
2022-04-19 23:45:10 -0400

    added code for taking screenshots in cypress test of projects page

--- Frontend/cypress/integration/projects.spec.ts
+++ Frontend/cypress/integration/projects.spec.ts
@@ -5,6 +5,7 @@ describe('Projects Page' , ()=> {
         cy.get('app-toolbar li').eq(4).click();
         cy.get('#user-items a').eq(1).click();
         cy.url().should('includes','projects');
+        cy.screenshot();
     })
     
 });
