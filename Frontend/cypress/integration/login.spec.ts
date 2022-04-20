65833a1837366e1a6579f22d57a81508ed57137a
1881praveen <46889521+1881praveen@users.noreply.github.com>
2022-04-19 22:22:26 -0400

    added cypress lines in commands.ts file to reduce repated lines for other cypress tests

--- Frontend/cypress/integration/login.spec.ts
+++ Frontend/cypress/integration/login.spec.ts
@@ -1,20 +1,15 @@
 describe('Login component', () => {
-  it('should Not Login if the form details are invalid beacuse wrong email is entered', () => {
+  it('should Not Login if the form details are invalid', () => {
     cy.visit('/login');
     cy.url().should('includes','login');
     cy.get('[formControlName="email"]').type('wrongemail@gmail.com');
-    cy.get('[formControlName="password"]').type('password');
-    cy.get('#login-button').click();
+    cy.get('#login-button').should('be.disabled');
     cy.url().should('include','/login');
     cy.screenshot();
   });
 
   it('should Login if the form details are valid because entries are validated correctly', () => {
-    cy.visit('/login');
-    cy.url().should('includes','login');
-    cy.get('[formControlName="email"]').type('nirupamyashas@gmail.com');
-    cy.get('[formControlName="password"]').type('password');
-    cy.get('#login-button').click();
+    cy.login('nirupamyashas@gmail.com','password');
     cy.url().should('include','/');
     cy.screenshot();
   });
